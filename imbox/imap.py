e33ac68cb9c162048f5b92282daf4e4ccdb1869a
Daniel Roberts <droberts@dimagi.com>
2013-11-23 19:27:56 -0300

    fix whitespace to comply with pep8
    - tabs => 4 spaces (including readme)
    - two newlines separating top-level declarations
    - one newline separating nested declarations
    - one newline at end of file
    - no whitespace at the end of lines
    - single space following (only) certain operators

--- imbox/imap.py
+++ imbox/imap.py
@@ -2,30 +2,27 @@ from imaplib import IMAP4, IMAP4_SSL
 
 
 class ImapTransport(object):
-	
-	def __init__(self, hostname, port=None, ssl=False):
-		self.hostname = hostname
-		self.port = port
-		
-		if ssl:
-			self.transport = IMAP4_SSL
-			if not self.port:
-				self.port = 993
-		else:
-			self.transport = IMAP4
-			if not self.port:
-				self.port = 143
+    
+    def __init__(self, hostname, port=None, ssl=False):
+        self.hostname = hostname
+        self.port = port
+        
+        if ssl:
+            self.transport = IMAP4_SSL
+            if not self.port:
+                self.port = 993
+        else:
+            self.transport = IMAP4
+            if not self.port:
+                self.port = 143
 
+    def list_folders(self):
+        return self.server.list()
 
-	def list_folders(self):
-		return self.server.list()
+    def connect(self, username, password):
+        self.server = self.transport(self.hostname, self.port)
+        self.server.login(username, password)
 
-	def connect(self, username, password):
-		self.server = self.transport(self.hostname, self.port)
-		self.server.login(username, password)
+        self.server.select()
 
-		self.server.select()
-
-		return self.server
-	
-
+        return self.server
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 import os
 
+
 version = '0.5.5'
 
 
@@ -8,18 +9,17 @@ def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 setup(
-	name='imbox',
-	version=version,
-	description="Python IMAP for Human beings",
-	long_description= read('README.md'),
-	keywords='email, IMAP, parsing emails',
-	author='Martin Rusev',
-	author_email='martinrusev@live.com',
-	url='https://github.com/martinrusev/imbox',
-	license='MIT',
-	packages=['imbox'],
-	package_dir={'imbox':'imbox'},
-	zip_safe=False,
-	install_requires=[],
-
-) 
+    name='imbox',
+    version=version,
+    description="Python IMAP for Human beings",
+    long_description=read('README.md'),
+    keywords='email, IMAP, parsing emails',
+    author='Martin Rusev',
+    author_email='martinrusev@live.com',
+    url='https://github.com/martinrusev/imbox',
+    license='MIT',
+    packages=['imbox'],
+    package_dir={'imbox': 'imbox'},
+    zip_safe=False,
+    install_requires=[],
+)
