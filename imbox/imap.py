ccc4743c156ecdb14fab7eda05f9dd9389462769
Dennis Schmalacker <github@progde.de>
2015-06-12 22:42:15 +0200

    Add logging to imbox

--- imbox/imap.py
+++ imbox/imap.py
@@ -1,5 +1,7 @@
 from imaplib import IMAP4, IMAP4_SSL
 
+import logging
+logger = logging.getLogger(__name__)
 
 class ImapTransport(object):
 
@@ -17,12 +19,14 @@ class ImapTransport(object):
                 self.port = 143
 
         self.server = self.transport(self.hostname, self.port)
+        logger.debug("Created IMAP4 transport for {host}:{port}".format(host=self.hostname, port=self.port))
 
     def list_folders(self):
+        logger.debug("List all folders in mailbox")
         return self.server.list()
 
     def connect(self, username, password):
         self.server.login(username, password)
         self.server.select()
-
+        logger.debug("Logged into server {} and selected mailbox 'INBOX'".format(self.hostname))
         return self.server
--- imbox/utils.py
+++ imbox/utils.py
@@ -1,8 +1,12 @@
 from __future__ import unicode_literals
 from six import PY3
 
+import logging
+logger = logging.getLogger(__name__)
+
 if PY3:
     def str_encode(value='', encoding=None, errors='strict'):
+        logger.debug("Encode str {} with and errors {}".format(value, encoding, errors))
         return str(value, encoding, errors)
 
     def str_decode(value='', encoding=None, errors='strict'):
