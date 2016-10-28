52f3518cd2c6051231e4dacba3296e31a4cd2474
Martin Rusev <martinrusev@users.noreply.github.com>
2016-10-28 10:52:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     install_requires=['six'],
     classifiers=(
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
