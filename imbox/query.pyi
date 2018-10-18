27d19a209a90c5580b369cb69fdda0ff7e70ac92
zevav <zev@averba.ch>
2018-10-18 13:00:42 -0400

    moved build_search_query to Messages, refactored to use a class attribute of IMAP_ATTRIBUTE_LOOKUP, so that the vendors package can overwrite and add entries to it for, for example, Gmail's IMAP extensions.  added X-GM-RAW to GmailMessages' copy of the lookup to make partial subject searches work.

--- imbox/query.pyi
+++
@@ -1,7 +0,0 @@
-import datetime
-from typing import Union
-
-
-def format_date(date: Union[str, datetime.date]) -> str: ...
-
-def build_search_query(**kwargs: Union[bool, str, datetime.date]) -> str: ...
